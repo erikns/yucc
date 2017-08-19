140c00ac9456062c902a298336d833e89afee8cf
Erik Sørensen <eriksorensen88@gmail.com>
2017-08-20 00:44:31 +0200

    First iteration. Read config file with credentials, and list zones gotton from API

---
+++ setup.py
@@ -0,0 +1,34 @@
+from codecs import open
+from os.path import abspath, dirname, join
+from subprocess import call
+
+from setuptools import Command, find_packages, setup
+
+from yucc import __version__
+
+this_dir = abspath(dirname(__file__))
+with open(join(this_dir, 'README.md'), encoding='utf-8') as file:
+    long_description = file.read()
+
+setup(
+    name = 'yucc',
+    version = __version__,
+    description = 'CLI for UpCloud public cloud service',
+    long_description = long_description,
+    url = 'TODO',
+    author = 'Erik Sørensen',
+    author_email = 'erik@enssoftware.no',
+    license = 'MIT',
+    classifiers = [
+    ],
+    keywords = 'cli upcloud',
+    packages = find_packages(exclude=['docs', 'tests*']),
+    install_requires = ['upcloud-api'],
+    extras_require = {},
+    entry_points = {
+        'console_scripts': [
+            'yucc-cli=yucc.cli:main',
+        ],
+    }
+)
+
---
+++ yucc/cli.py
@@ -0,0 +1,30 @@
+import upcloud_api
+from upcloud_api import ZONE
+import ConfigParser
+import os
+
+def map_section(config, section):
+    res = {}
+    options = config.options(section)
+    for option in options:
+        try:
+            res[option] = config.get(section, option)
+        except:
+            print "Error!"
+    return res
+
+def read_credentials():
+    config = ConfigParser.RawConfigParser()
+    config.read(os.path.join(os.path.expanduser('~'), '.yuccrc'))
+    creds = map_section(config, 'default')
+    print creds
+    return {'username': creds['username'],
+            'password': creds['password']}
+
+def main():
+    creds = read_credentials()
+    manager = upcloud_api.CloudManager(creds['username'], creds['password'])
+    zones = manager.get_zones()['zones']['zone']
+    for zone in zones:
+        print zone['id'], zone['description']
+
