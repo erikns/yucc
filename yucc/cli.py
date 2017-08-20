02f7478e4886d0f53b1deff922920b06565edf9f
Erik Sørensen <eriksorensen88@gmail.com>
2017-08-20 19:01:22 +0200

    Add flag to print version info and exit

--- yucc/cli.py
+++ yucc/cli.py
@@ -4,13 +4,17 @@ import argparse
 
 from .config import read_credentials
 from .logger import Logger, LogLevel
+from . import __version__
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='UpCloud CLI')
     parser.add_argument('-v', '--verbose', action = 'count', 
             dest = 'log_level', default = 0,
             help = 'be more verbose in logging')
+    parser.add_argument('--version', action = 'store_true',
+            dest = 'show_version', default = False,
+            help = 'print version information and exit')
 
     args = parser.parse_args()
     args.log_level = LogLevel.ERROR + args.log_level
@@ -20,9 +24,11 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-
     logger = Logger(args.log_level)
     logger.debug('Arguments: ' + str(args))
+    if args.show_version:
+        logger.normal('yucc-cli version ' + __version__)
+        exit(0)
 
     logger.debug('Reading credentials from preference file')
     creds = read_credentials()
