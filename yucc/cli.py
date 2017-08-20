1d624aad77a94f498ae4691a3f7003e257328959
Erik Sørensen <eriksorensen88@gmail.com>
2017-08-20 02:05:00 +0200

    Add argument to set verbosity level

--- yucc/cli.py
+++ yucc/cli.py
@@ -1,11 +1,28 @@
 import upcloud_api
 from upcloud_api import ZONE
+import argparse
 
 from .config import read_credentials
 from .logger import Logger, LogLevel
 
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', action = 'count', 
+            dest = 'log_level', default = 0,
+            help = 'be more verbose in logging')
+
+    args = parser.parse_args()
+    args.log_level = LogLevel.ERROR + args.log_level
+    
+    return args
+
+
 def main():
-    logger = Logger(LogLevel.DEBUG)
+    args = parse_arguments()
+
+    logger = Logger(args.log_level)
+    logger.debug('Arguments: ' + str(args))
 
     logger.debug('Reading credentials from preference file')
     creds = read_credentials()
