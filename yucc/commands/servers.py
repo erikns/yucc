dc29cbe472f9d2e4e79eb80be26b9928d43a92e6
Erik Sørensen <eriksorensen88@gmail.com>
2017-08-22 01:30:02 +0200

    Simple implementation of listing servers

--- yucc/commands/servers.py
+++ yucc/commands/servers.py
@@ -2,12 +2,16 @@ import upcloud_api
 
 from .common import upcloud_api_call
 from ..logger import Logger
+from ..outputter import output
 
 class Servers:
     def __init__(self, log_level, creds):
         self.logger = Logger(log_level)
+        self.creds = creds
 
+    @upcloud_api_call
     def run(self):
-        self.logger.critical('Command to list servers is not implemented yet')
-        return False
+        manager = upcloud_api.CloudManager(self.creds['username'], self.creds['password'])
+        servers = manager.get_servers()
+        output('server', servers)
 
--- yucc/outputter.py
+++ yucc/outputter.py
@@ -1,9 +1,12 @@
 
 def _format_from_kind(kind):
     if kind == 'zone':
-        return lambda x: '{}  {}'.format(x['id'], x['description'])
+        return lambda x: '{}  {}'.format(x['id'], x['description']), None
     elif kind == 'template':
-        return lambda x: '{}  {}'.format(x['uuid'], x['title'])
+        return lambda x: '{}  {}'.format(x['uuid'], x['title']), None
+    elif kind == 'server':
+        return lambda x: '{}  {}  {}  {}MB RAM  {}CPU  {}'.format(x.uuid, x.hostname, x.title,
+                    x.memory_amount, x.core_number, x.state)
     else:
         raise Error
 
