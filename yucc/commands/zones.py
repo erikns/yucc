857e906a0f29d9292b5579004a310e4cf5e23983
Erik Sørensen <eriksorensen88@gmail.com>
2017-08-22 00:12:16 +0200

    Handle authentication error in ls zones

--- yucc/commands/zones.py
+++ yucc/commands/zones.py
@@ -8,11 +8,21 @@ class Zones:
         self.creds = creds
 
     def run(self):
-        manager = upcloud_api.CloudManager(self.creds['username'], self.creds['password'])
+        try:
+            manager = upcloud_api.CloudManager(self.creds['username'], self.creds['password'])
 
-        self.logger.debug('Collecting zones...')
-        zones = manager.get_zones()['zones']['zone']
-        self.logger.debug('Zones collected')
+            self.logger.debug('Collecting zones...')
+            zones = manager.get_zones()['zones']['zone']
+            self.logger.debug('Zones collected')
 
-        output('zone', zones)
+            output('zone', zones)
+        except upcloud_api.errors.UpCloudAPIError as error:
+            if error.error_code == 'AUTHENTICATION_FAILED':
+                self.logger.error('Authentication failed')
+            else:
+                self.logger.error('Unknown error occurred: ' +
+                        error.error_message)
+            return False
+
+        return True
 
