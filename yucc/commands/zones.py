bcd498e4d13273f11f655b84b99d76c9d159847f
Erik Sørensen <eriksorensen88@gmail.com>
2017-08-22 00:35:34 +0200

    Factor out commond error handling into decorator

--- yucc/commands/zones.py
+++ yucc/commands/zones.py
@@ -1,28 +1,20 @@
 import upcloud_api
 from ..logger import Logger
 from ..outputter import output
+from .common import upcloud_api_call
 
 class Zones:
     def __init__(self, log_level, creds):
         self.logger = Logger(log_level)
         self.creds = creds
 
+    @upcloud_api_call
     def run(self):
-        try:
-            manager = upcloud_api.CloudManager(self.creds['username'], self.creds['password'])
+        manager = upcloud_api.CloudManager(self.creds['username'], self.creds['password'])
 
-            self.logger.debug('Collecting zones...')
-            zones = manager.get_zones()['zones']['zone']
-            self.logger.debug('Zones collected')
+        self.logger.debug('Collecting zones...')
+        zones = manager.get_zones()['zones']['zone']
+        self.logger.debug('Zones collected')
 
-            output('zone', zones)
-        except upcloud_api.errors.UpCloudAPIError as error:
-            if error.error_code == 'AUTHENTICATION_FAILED':
-                self.logger.error('Authentication failed')
-            else:
-                self.logger.error('Unknown error occurred: ' +
-                        error.error_message)
-            return False
-
-        return True
+        output('zone', zones)
 
