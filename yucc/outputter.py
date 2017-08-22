449e116ae711f5bd9b7c043404930fbf1140ec97
Erik Sørensen <eriksorensen88@gmail.com>
2017-08-22 23:02:03 +0200

    Fix formatting issue. Revert partial experimental implementation

--- yucc/outputter.py
+++ yucc/outputter.py
@@ -1,9 +1,9 @@
 
 def _format_from_kind(kind):
     if kind == 'zone':
-        return lambda x: '{}  {}'.format(x['id'], x['description']), None
+        return lambda x: '{}  {}'.format(x['id'], x['description'])
     elif kind == 'template':
-        return lambda x: '{}  {}'.format(x['uuid'], x['title']), None
+        return lambda x: '{}  {}'.format(x['uuid'], x['title'])
     elif kind == 'server':
         return lambda x: '{}  {}  {}  {}MB RAM  {}CPU  {}'.format(x.uuid, x.hostname, x.title,
                     x.memory_amount, x.core_number, x.state)
