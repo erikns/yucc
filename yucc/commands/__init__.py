c56c9897c57238e44984ab7c882d877c24bd0640
Erik Sørensen <eriksorensen88@gmail.com>
2017-08-30 19:58:33 +0200

    Remove dead imports in old commands module

--- yucc/commands/__init__.py
+++ yucc/commands/__init__.py
@@ -1,6 +1,4 @@
 from .zones import list_zones
 from .templates import list_templates
-from .account import show_account_info
 from .servers import *
 from .plans import list_plans
-from .profile import dump_profile_info
