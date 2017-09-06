f3609e5cd5a8600c16d3fe7cc1f7b9740de41357
Erik Sørensen <eriksorensen88@gmail.com>
2017-09-06 15:38:59 +0200

    Use RST for readme on PyPI

---
+++ README.rst
@@ -0,0 +1,6 @@
+======================
+yucc
+======================
+
+yucc is a command line client to the UpCloud cloud service at upcloud.com. Still
+a work in progress.
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import Command, find_packages, setup
 from yucc import __version__, __prog__
 
 this_dir = abspath(dirname(__file__))
-with open(join(this_dir, 'README.md'), encoding='utf-8') as file:
+with open(join(this_dir, 'README.rst'), encoding='utf-8') as file:
     long_description = file.read()
 
 setup(
